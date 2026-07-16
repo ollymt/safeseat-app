86f7276ff1eb6671ab1370fe41247639ed6fa2e4
ooniins <gabrielcasanovaq@gmail.com>
2026-07-16 20:04:28 +0800

    Fix: Search Bar and bugs when clicking + button

--- src/app/(tabs)/everyone/index.tsx
+++ src/app/(tabs)/everyone/index.tsx
@@ -3,16 +3,16 @@ import { Button, Host, Icon, Row, TextInput } from "@expo/ui";
 import { GlassView } from "expo-glass-effect";
 import { useFocusEffect, useRouter } from "expo-router";
 import {
-	Dimensions,
-	FlatList,
-	KeyboardAvoidingView,
-	Platform,
-	Pressable,
-	SectionList,
-	StyleSheet,
-	Text,
-	useColorScheme,
-	View,
+  Dimensions,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  SectionList,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -30,9 +30,9 @@ import ContactCard from "@/components/contact-card";
 import ProfileCard from "@/components/profile-card";
 import SegmentedControl from "@expo/ui/community/segmented-control";
 import {
-	buttonBorderShape,
-	buttonStyle,
-	controlSize,
+  buttonBorderShape,
+  buttonStyle,
+  controlSize,
 } from "@expo/ui/swift-ui/modifiers";
 
 const { width: screenWidth } = Dimensions.get("window");
@@ -440,7 +440,7 @@ export default function Everyone() {
           keyboardVerticalOffset={50}
           style={{
             position: "absolute",
-            bottom: 100,
+            bottom: Platform.OS === "ios" ? 100 : 20,
             left: 20,
             right: 20,
             zIndex: 10,
